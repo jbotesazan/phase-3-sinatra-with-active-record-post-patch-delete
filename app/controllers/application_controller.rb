0b012e96c8f1a299c3ba360db0baf12836e9e5b0
Jason Botesazan <jbotesazan@gmail.com>
2022-01-26 17:48:47 -0500

    commit tree 4ed7bddba3fedcae733c725fe41589e7707e5494

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,35 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  patch '/reviews/:id' do
+    review_update = Review.find(params[:id])
+    review_update.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review_update.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review
+    review.destroy
+    # send a response with the deleted review as JSON
+    review.to_json
+  end
 
   get '/games' do
     games = Game.all.order(:title).limit(10)
